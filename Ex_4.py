352303e77eb1c2c470036d46468254e80f7cf0d8
Muminov Kamoljon <kamol.mumino.786@gmail.com>
2022-10-03 09:24:02 +0300

    Homework

--- Ex_4.py
+++ Ex_4.py
@@ -1,15 +1,1 @@
-#Напишите программу, которая принимает на вход 2 числа. Задайте список из N элементов,
-#  заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных 
-# позициях(не индексах).
-
-num = int( input('Enter the value of N: ' ))
-n_1 = int(input('Position 1: '))
-n_2 = int(input('Position 2: '))
-list_num = list(range(-num, num +1))
-print(list_num)
-len_list = len(list_num)
-if len_list >= n_1 > 0 and len_list >= n_2 > 0:
-        print(list_num[n_1 -1] * list_num[n_2 - 1])
-else:
-   print('There are no values for these indexes! ')
 
